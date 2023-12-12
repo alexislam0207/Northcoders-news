6c24a9c5f4543d765067a0c6b9ff2a6a8294c986
alexislam0207 <alexislam0207@gmail.com>
2023-12-12 11:31:26 +0000

    resolve merge conflict

--- src/api.js
+++ src/api.js
@@ -33,32 +33,34 @@ export const getComments = (article_id) => {
 };
 
 export const updateVote = (article_id, vote) => {
-  return fetch(`https://alexis-news-server.onrender.com/api/articles/${article_id}`, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ inc_votes: vote }),
-  });
+  return fetch(
+    `https://alexis-news-server.onrender.com/api/articles/${article_id}`,
+    {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ inc_votes: vote }),
+    }
+  );
 };
 
-}
-
-
-
-export const postComment = (article_id, username, body)=>{
-  return fetch(`https://alexis-news-server.onrender.com/api/articles/${article_id}/comments`,{
-    method: 'POST',
-    headers:{'Content-Type':'application/json',},
-    body: JSON.stringify({
-      username: username,
-      body: body,
-    }),
-  })
-  .then((res) => {
-    return res.json();
-  })
-  .then((data) => {
-    return data.comment;
-  });
-}
+export const postComment = (article_id, username, body) => {
+  return fetch(
+    `https://alexis-news-server.onrender.com/api/articles/${article_id}/comments`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username: username,
+        body: body,
+      }),
+    }
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      return data.comment;
+    });
+};
